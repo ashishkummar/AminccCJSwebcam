0433b8c98d79d1be7985597bea62405de8644340
Ashish Kumar <ashishkummar@gmail.com>
2017-03-27 18:48:29 +0530

    Update animccwebcam.js

--- animccwebcam.js
+++ animccwebcam.js
@@ -1,9 +1,13 @@
-var targetMC = this.box;
+/*
+version 1.0
+By- ashishkummar@gmail.com
+*/
+var targetMC = this.camRenderer;
 
 var _cjBitmap;
-var _camWidth  =  targetMC.nominalBounds.width;
-var	_camHeight =  targetMC.nominalBounds.height;
-var _morror=true;
+var _camWidth = targetMC.nominalBounds.width;
+var _camHeight = targetMC.nominalBounds.height;
+var _morror = true;
 
 
 navigator.getUserMedia = navigator.getUserMedia ||
@@ -14,43 +18,43 @@ navigator.getUserMedia = navigator.getUserMedia ||
 if (!navigator.getUserMedia) {
 	return false;
 }
+
+
+var video = document.createElement('video'),
+	track;
+video.setAttribute('autoplay', true);
+
+
+
+navigator.getUserMedia({
+	video: true,
+	audio: false
+}, function (stream) {
+	video.src = window.URL.createObjectURL(stream);
+	track = stream.getTracks()[0];
+}, function (e) {
+	console.error('Rejected!', e);
+});
+
+
+
+_cjBitmap = new createjs.Bitmap(video);
+
+if (_morror) {
+	_cjBitmap.scaleX = -1;
+	_cjBitmap.x = _camWidth;
+}
+
+
+
+targetMC.addChild(_cjBitmap);
+
  
+var loopFrame;
 
-var video = document.createElement('video'), track;
-	video.setAttribute('autoplay', true);
- 
-	   
-
-	navigator.getUserMedia({
-		video: true,
-		audio: false
-	}, function (stream) {
-		video.src = window.URL.createObjectURL(stream);
-		track = stream.getTracks()[0];
-	}, function (e) {
-		console.error('Rejected!', e);
-	});
-
-var  loopFrame; 
-	 
 function loop() {
-	
-	loopFrame = requestAnimationFrame(loop); 
- 
-	_cjBitmap = new createjs.Bitmap(video);
-	
-	 if(_morror){
- 	   _cjBitmap.scaleX = -1;
- 	   _cjBitmap.x = _camWidth;
-	 }
 
-	if (targetMC.numChildren >= 4) {
-		for (var t = 0; t < targetMC.numChildren; t++) {
-			targetMC.removeChildAt(t);
-		}
-	} 
- 
-	 targetMC.addChild(_cjBitmap);
+	loopFrame = requestAnimationFrame(loop);
  
 }
 
